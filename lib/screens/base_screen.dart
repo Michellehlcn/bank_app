b5ab7109e4ef4ddc93644ddab3ab16e8851dd8b7
michellehlcn <michellehlcn.au@gmail.com>
2024-06-12 14:40:26 +1000

    init app

---
+++ lib/screens/base_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bank_app/constants/color_constants.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class BaseScreen extends StatefulWidget {
+  const BaseScreen({Key? key}) : super(key: key);
+
+  @override
+  _BaseScreenState createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = [
+    const HomeScreen(),
+    const CardsScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: kPrimaryColor,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.house), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.creditCard), label: 'Cards'),
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.gear), label: 'Settings'),
+          BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.chartBar), label: 'Overview'),
+        ],
+      ),
+    );
+  }
+}
