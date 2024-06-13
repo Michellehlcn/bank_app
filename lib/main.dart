8747c4cce535cfc78c9484605fa2b10395d7142a
michellehlcn <michellehlcn.au@gmail.com>
2024-06-13 21:13:21 +1000

    init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,15 @@ import 'package:flutter_bank_app/screens/base_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
